115b7fc7aba693b3e06ad132967f43f4f91cc5b3
Rohit Raj <rohitrsa@amazon.com>
2022-12-22 10:52:21 +0530

    Converted Data Element to AVRO GenericRecord

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,10 @@ import com.amazonaws.services.schemaregistry.utils.AvroRecordType;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.reflect.ReflectData;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -35,10 +39,24 @@ public class Main {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "b-1.gommtsource.s5acw7.c18.kafka.us-east-1.amazonaws.com:9092,b-2.gommtsource.s5acw7.c18.kafka.us-east-1.amazonaws.com:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        try (KafkaProducer<String, TripDataVO> producer = new KafkaProducer<>(props)) {
+        Schema tripSchema = ReflectData.get().getSchema(TripDataVO.class);
+
+        try (KafkaProducer<String, GenericRecord> producer = new KafkaProducer<>(props)) {
             while(mappingIterator.hasNext()){
+                GenericRecord tripRecordGeneric = new GenericData.Record(tripSchema);
                 TripDataVO tripDataElement= mappingIterator.next();
-                final ProducerRecord<String,TripDataVO> record = new ProducerRecord<>(TOPIC,tripDataElement.getId().toString(),tripDataElement);
+                tripRecordGeneric.put("id",tripDataElement.getId());
+                tripRecordGeneric.put("vendor_id",tripDataElement.getVendor_id());
+                tripRecordGeneric.put("pickup_datetime",tripDataElement.getPickup_datetime());
+                tripRecordGeneric.put("dropoff_datetime",tripDataElement.getDropoff_datetime());
+                tripRecordGeneric.put("passenger_count",tripDataElement.getPassenger_count());
+                tripRecordGeneric.put("pickup_longitude",tripDataElement.getPickup_longitude());
+                tripRecordGeneric.put("pickup_latitude",tripDataElement.getPickup_latitude());
+                tripRecordGeneric.put("dropoff_longitude",tripDataElement.getDropoff_longitude());
+                tripRecordGeneric.put("dropoff_latitude",tripDataElement.getDropoff_latitude());
+                tripRecordGeneric.put("store_and_fwd_flag",tripDataElement.getStore_and_fwd_flag());
+                tripRecordGeneric.put("trip_duration",tripDataElement.getTrip_duration());
+                final ProducerRecord<String,GenericRecord> record = new ProducerRecord<>(TOPIC,tripRecordGeneric.get("id").toString(),tripRecordGeneric);
                 producer.send(record);
                 System.out.println("sent Record with id: "+tripDataElement.id);
             }
